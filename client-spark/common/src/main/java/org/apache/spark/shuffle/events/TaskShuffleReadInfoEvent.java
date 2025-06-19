fcefd3dc3f5534c0a4536ff1833e4cb1c530448b
Junfan Zhang <zuston@apache.org>
2025-06-19 14:11:06 +0800

    [#2508] feat(spark3): Record failed tasks on any shuffle write/failure into event logs

--- client-spark/common/src/main/java/org/apache/spark/shuffle/events/TaskShuffleReadInfoEvent.java
+++ client-spark/common/src/main/java/org/apache/spark/shuffle/events/TaskShuffleReadInfoEvent.java
@@ -24,13 +24,22 @@ public class TaskShuffleReadInfoEvent extends UniffleEvent {
   private int shuffleId;
   private long taskId;
   private Map<String, ShuffleReadMetric> metrics;
+  private boolean isShuffleReadFailed;
+  private String failureReason;
 
   public TaskShuffleReadInfoEvent(
-      int stageId, int shuffleId, long taskId, Map<String, ShuffleReadMetric> metrics) {
+      int stageId,
+      int shuffleId,
+      long taskId,
+      Map<String, ShuffleReadMetric> metrics,
+      boolean isShuffleReadFailed,
+      String failureReason) {
     this.stageId = stageId;
     this.shuffleId = shuffleId;
     this.taskId = taskId;
     this.metrics = metrics;
+    this.isShuffleReadFailed = isShuffleReadFailed;
+    this.failureReason = failureReason;
   }
 
   public int getStageId() {
