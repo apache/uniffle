3651412297e1f79c9e31e6a6dad45df6b1bd73e2
Junfan Zhang <zuston@apache.org>
2025-10-29 15:25:18 +0800

    enable validation by default

--- client-spark/common/src/main/java/org/apache/spark/shuffle/RssSparkConfig.java
+++ client-spark/common/src/main/java/org/apache/spark/shuffle/RssSparkConfig.java
@@ -42,8 +42,8 @@ public class RssSparkConfig {
   public static final ConfigOption<Boolean> RSS_CLIENT_INTEGRITY_VALIDATION_ENABLED =
       ConfigOptions.key("rss.client.integrityValidation.enabled")
           .booleanType()
-          .defaultValue(false)
-          .withDescription("Whether or not to enable shuffle data integrity validation mechanism");
+          .defaultValue(true)
+          .withDescription("Whether or not to enable shuffle data integrity validation mechanism (spark version >= 3.5.0)");
 
   public static final ConfigOption<Boolean> RSS_DATA_INTEGRATION_VALIDATION_ANALYSIS_ENABLED =
       ConfigOptions.key("rss.client.integrityValidation.failureAnalysisEnabled")
