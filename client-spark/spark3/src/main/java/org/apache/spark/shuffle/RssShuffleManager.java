51b7253c5d832eb86854ad8e8f0ab88cd2946fee
Junfan Zhang <zuston@apache.org>
2025-11-13 16:24:41 +0800

    fix

--- client-spark/spark3/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
+++ client-spark/spark3/src/main/java/org/apache/spark/shuffle/RssShuffleManager.java
@@ -17,7 +17,12 @@
 
 package org.apache.spark.shuffle;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
